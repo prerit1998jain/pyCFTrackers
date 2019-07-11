0c6361d592af66dd41c5e686a66f5bcbf2b8fbb6
prerit1998jain <preritjain1234@gmail.com>
2019-07-12 04:52:45 +0530

    Comments added

--- examples/cf_demo.py
+++ examples/cf_demo.py
@@ -2,13 +2,21 @@ import os
 from examples.pytracker import PyTracker
 from lib.utils import get_ground_truthes,plot_precision,plot_success
 from examples.otbdataset_config import OTBDatasetConfig
-if __name__ == '__main__':
-    data_dir='../dataset/test'
-    data_names=sorted(os.listdir(data_dir))
 
+if __name__ == '__main__':
+    # Data to be installed.
+    data_dir='../dataset/test'
+
+    # Making a sorted list of the data files in the particular path
+    data_names=sorted(os.listdir(data_dir))
     print(data_names)
+
+    # An instance of the class defined in otbdataset.config
     dataset_config=OTBDatasetConfig()
+
+    # traversing over the data_names
     for data_name in data_names:
+
         data_path=os.path.join(data_dir,data_name)
         gts = get_ground_truthes(data_path)
         if data_name in dataset_config.frames.keys():
