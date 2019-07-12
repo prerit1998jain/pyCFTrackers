de9f340241c70fe6742c6f6c59cd85bcec0990f1
prerit1998jain <preritjain1234@gmail.com>
2019-07-12 14:37:22 +0300

    comments completed

--- examples/cf_demo.py
+++ examples/cf_demo.py
@@ -14,17 +14,29 @@ if __name__ == '__main__':
     # An instance of the class defined in otbdataset.config
     dataset_config=OTBDatasetConfig()
 
-    # traversing over the data_names
+    # Traversing over the data_names
     for data_name in data_names:
 
-        data_path=os.path.join(data_dir,data_name)
+        # Identifying the path to a particular file.
+        data_path = os.path.join(data_dir,data_name)
+
+        # Getting the ground_truth in the form of an np array
         gts = get_ground_truthes(data_path)
+
+        #  Why is it?
         if data_name in dataset_config.frames.keys():
             start_frame,end_frame=dataset_config.frames[data_name][:2]
             if data_name!='David':
                 gts=gts[start_frame-1:end_frame]
+
+        # Identifying the directory consisting of the images.
         img_dir = os.path.join(data_path,'img')
+
+        # Tracker is defined (STRCF is used here.)
         tracker = PyTracker(img_dir,tracker_type='STRCF',dataset_config=dataset_config)
+
+        # tracking method returns the coordinates of the bounding boxes in the furthur frames
         poses=tracker.tracking(verbose=True,video_path=os.path.join('../results/CF',data_name+'_vis.avi'))
+        
         plot_success(gts,poses,os.path.join('../results/CF',data_name+'_success.jpg'))
         plot_precision(gts,poses,os.path.join('../results/CF',data_name+'_precision.jpg'))
